4d062e6c351563c482b53b29bbaec3fd6ad7d563
bridgerwinter <bridgerwinter@gmail.com>
2024-07-12 16:00:19 -1000

    Add project files.

---
+++ BridgerFit/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace BridgerFit
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
